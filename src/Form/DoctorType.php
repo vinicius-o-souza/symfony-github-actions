b76f854e2ec0ba1c9b5b0ecd1e0c63442988be04
Vinícius de Oliveira Souza <vinicius.souza@justdigital.com.br>
2020-09-01 18:37:21 -0300

    Commit Initial

---
+++ src/Form/DoctorType.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Doctor;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class DoctorType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Doctor::class,
+        ]);
+    }
+}
